d01e537019ece9ea4f014167426681ff6f1ba42b
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-05-10 16:14:29 +1200

    Basic Routing There is now some basic routing and an index page with controller

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 // Create the angular app 'goal'
-angular.module('goal', ['ngRoute']);
+angular.module('goal', ['ngRoute'])
+        .config(config);
+
+function config($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/index.html',
+        controller: 'indexController'
+    });
+}
---
+++ js/controllers/IndexController.js
@@ -0,0 +1,5 @@
+angular.module('goal').controller('indexController', ['$scope', Index]);
+
+function Index($scope) {
+    
+}
