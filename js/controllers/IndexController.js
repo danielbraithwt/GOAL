7def01a9fd3ad05acecc4ea6283aee51b3604519
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-05-11 12:57:43 +1200

    Removing Goals Can now remove goals

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -7,9 +7,17 @@ function Controller($scope) {
     $scope.goalName = '';
     $scope.goals = [];
     $scope.addGoal = addGoal;
+    $scope.removeGoal = removeGoal;
 
+    ///////////////
+    // Functions //
+    
     function addGoal() {
         $scope.goals.push($scope.goalName);
         $scope.goalName = '';
+    }
+    
+    function removeGoal(index) {
+        $scope.goals.splice(index, 1);
     }
 };
