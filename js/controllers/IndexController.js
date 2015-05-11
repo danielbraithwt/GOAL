d3e2ac697fd17f8563797c5aa06cb7d18d5e6792
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-05-11 14:16:56 +1200

    Goal Importance Can now set the importance of goals

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -12,19 +12,43 @@
         var vm = this;
         vm.property = 'Controller';
         vm.goalName = '';
+        vm.goalImportance = '';
         vm.goals = [];
         vm.addGoal = addGoal;
         vm.removeGoal = removeGoal;
+		vm.getDayImportance = getDayImportance;
 
         ////////////////
 
+        // Adds a goal to the array of goals
         function addGoal() {
-            vm.goals.push(vm.goalName);
+            var goal = {};
+            goal.name = vm.goalName;
+            goal.importance = parseInt(vm.goalImportance);
+            
+            // Insert the new goal into goals
+            vm.goals.push(goal);
+            
+            // Reset the form fields
             vm.goalName = '';
+            vm.goalImportance = '';
         }
 
+        // Removes the goal at index from the array
+        // of goals
         function removeGoal(index) {
             vm.goals.splice(index, 1);
+        }
+        
+		// Get the importance of the current day
+        function getDayImportance() {
+            var total = 0;
+			
+			for (var i = 0; i < vm.goals.length; i++) {
+				total += vm.goals[i].importance;
+			}
+			
+			return total;
         }
     }
 })();
