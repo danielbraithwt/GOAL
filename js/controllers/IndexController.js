f6fa5d0000f99285e74285a54d9f4601df36bd0f
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-05-12 17:18:43 +1200

    Improved Done Goal Tracking Now each goal object has a done flag in it

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -14,10 +14,10 @@
         vm.goalName = '';
         vm.goalImportance = '';
         vm.goals = [];
-		vm.finishedGoals = [];
         vm.addGoal = addGoal;
         vm.removeSelectedGoals = removeSelectedGoals;
 		vm.markDoneSelectedGoals = markDoneSelectedGoals;
+		vm.isDone = isDone;
 		vm.getDayImportance = getDayImportance;
 		vm.getDayFinishedImportance = getDayFinishedImportance;
 
@@ -25,10 +25,12 @@
 
         // Adds a goal to the array of goals
         function addGoal() {
+			// Create a new goal
             var goal = {};
             goal.name = vm.goalName;
             goal.importance = parseInt(vm.goalImportance);
 			goal.selected = false;
+			goal.done = false;
             
             // Insert the new goal into goals
             vm.goals.push(goal);
@@ -50,14 +52,23 @@
 			}
         }
 		
+		// Marks all the selected goals as being
+		// done
 		function markDoneSelectedGoals() {
 			for (var i = 0; i < vm.goals.length; i++) {
 				
 				// If the goal is selected 
             	if (vm.goals[i].selected === true) {
-					vm.finishedGoals.push(vm.goals.splice(i, 1)[0]);
+					vm.goals[i].done = true;
+					vm.goals[i].selected = false;
 				}
 			}	
+		}
+		
+		// Given a goal object return wether it
+		// is done or not
+		function isDone(goal) {
+			return !goal.done;	
 		}
         
 		// Get the importance of the current day
@@ -68,16 +79,18 @@
 				total += vm.goals[i].importance;
 			}
 			
-			total += vm.getDayFinishedImportance();
-			
 			return total;
         }
 		
+		// Goes through the goals and adds up the
+		// importance of the goals you have finished
 		function getDayFinishedImportance() {
 			var total = 0;
 			
-			for (var i = 0; i < vm.finishedGoals.length; i++) {
-				total += vm.finishedGoals[i].importance;
+			for (var i = 0; i < vm.goals.length; i++) {
+				if (vm.goals[i].done) {
+					total += vm.goals[i].importance;
+				}
 			}
 
 			return total;
