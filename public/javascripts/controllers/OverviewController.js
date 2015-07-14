1fb3eb755a9af3223018877284a6efc0baf5d35e
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-07-14 21:09:47 +1200

    Complete List Of Grades The list of grades is now a propper one

--- public/javascripts/controllers/OverviewController.js
+++ public/javascripts/controllers/OverviewController.js
@@ -158,14 +158,26 @@
 		}
 
 		function percentageToGrade(grade) {
-			if (grade < 20) {
-				return "F";	
-			} else if (grade < 30) {
+			if (grade < 40) {
 				return "E";	
-			} else if (grade < 40) {
-				return "C-";	
 			} else if (grade < 50) {
+				return "D";	
+			} else if (grade < 55) {
+				return "C-";	
+			} else if (grade < 60) {
 				return "C";	
+			} else if (grade < 65) {
+				return "C+";	
+			} else if (grade < 70) {
+				return "B-";	
+			} else if (grade < 75) {
+				return "B";
+			} else if (grade < 80) {
+				return "B+";
+			} else if (grade < 85) {
+				return "A-";	
+			} else if (grade < 90) {
+				return "A";	
 			}
 
 			return "A+";
