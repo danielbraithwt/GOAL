a6402fa0b5710f1ea7c30ea22defc02dd1576406
Daniel Braithwaite <danielbraithwt@gmail.com>
2015-08-12 10:43:54 +1200

    SocketIO Service Syntax Fixed a syntax mistake in the socket io service that was causing the app to not work on some browsers

--- public/javascripts/services/SocketIOService.js
+++ public/javascripts/services/SocketIOService.js
@@ -10,21 +10,23 @@
 	function factory(){
 		var exports = {
 			emit: emit,
-			on, on,
+			on: on,
 		};
 
 		// Create a socket to the webserver
-		var socket = io();//io('http://localhost:3000');
+		var socket = io();
 		
 		return exports;
 
 		////////////////
 
+		// Emit the message with the data to the server
 		function emit(message, data) {
-			console.log("Emmiting: " + message + " -> " + data);
 			socket.emit(message, data);
 		}
 		
+		// When a socket recives the message
+		// run the callback passed in
 		function on(message, callback) {
 			socket.on(message, callback);
 		}
